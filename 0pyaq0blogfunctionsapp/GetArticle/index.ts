621cc54ca939d392201a32e2bf00adfbc62b8cb1
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-08-27 17:29:28 +0900

    ⚡ :: 단일 게시물 골라오기

--- 0pyaq0blogfunctionsapp/GetArticle/index.ts
+++ 0pyaq0blogfunctionsapp/GetArticle/index.ts
@@ -1,17 +1,28 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import articlesService from "../services/articlesService";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest,
+): Promise<void> {
+  let response;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  try {
 
+    //호출주소(url)로 전달되는 게시글의 고유번호
+    //domain.com/article/1
+    const id = req.params.id;
+
+    //http본문으로 전달되는 json데이터
+    const searchData = req.body;
+    
+    let article = await articlesService.read(id,searchData.ipaddress);
+    response = { body: article, status: 200 };
+  } catch (err) {
+    response = { body: err.message, status: 500 };
+  }
+
+  context.res = response;
 };
 
 export default httpTrigger;
